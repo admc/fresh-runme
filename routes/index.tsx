464e35154ce2d749d004d9b7c9c5fd52b6c62c60
Adam Christian <adam.christian@gmail.com>
2022-10-03 13:52:32 -0700

    Add gif

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,18 +1,19 @@
-import Counter from "../islands/Counter.tsx";
-
 export default function Home() {
   return (
-    <div class="p-4 mx-auto max-w-screen-md">
-      <img
-        src="/logo.svg"
-        class="w-32 h-32"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class="my-6">
-        Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
+    <div class="bg-black text-white w-full min-h-screen">
+      <div class="p-4 mx-auto max-w-screen-xl flex flex-col space-y-16 items-center justify-center">
+        <div class="text-center text-7xl">runme 🛸</div>
+        <div class="">
+          <div class="bg-green-500 rounded shadow  px-4 py-1">Install the extension</div>
+        </div>
+        <div>
+          <img
+            src="/demo.gif"
+            class="min-w-max"
+            alt="the fresh logo: a sliced lemon dripping with juice"
+          />
+        </div>
+      </div>
     </div>
   );
 }
